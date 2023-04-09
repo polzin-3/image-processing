555f4267eec95314a7cffd30efbd4e4cc04c18c1
Elliott Polzin <elliott.polzin3@gmail.com>
2023-04-09 10:19:40 +0100

    initial refactor

---
+++ src/imageprocess/align/align.py
@@ -0,0 +1,201 @@
+from typing import List
+
+from pipeline import utils, image_funcs, plot_funcs
+import os
+import numpy as np
+import pandas as pd
+import cv2
+import rasterio
+import ntpath  # for file naming
+
+
+class ImageAlign:
+    """Uses image feature matching to warp and align mosaic images created from
+    images taken in different flight campaigns. This is useful for mosaics
+    created without the use of ground control points, as the resulting
+    georeferenced mosaics will have some error that does not correlate between
+    campaigns."""
+    def __init__(
+            self,
+            feature_method: str = 'akaze',
+            detection_threshold: float = 0.001
+    ):
+        """Initiate class with feature detection method and feature detection
+        threshold
+
+        Parameters
+        ----------
+        feature_method: str, optional
+            Image feature detection method. Currently only AKAZE implemented
+            (https://docs.opencv.org/4.1.0/d8/d30/classcv_1_1AKAZE.html)
+        detection_threshold: float, optional
+            Decimal threshold to use to determine "high quality" features,
+            lower values result in more features but many may be noisy and could
+            result in more false matches during feature matching
+        """
+        self.feature_method = feature_method
+        self.detection_threshold = detection_threshold
+
+    def detect_features(self, images: list) -> None:
+        """Detect features in all images. If images are not already greyscale
+        and uint8, then they will be converted prior to detection
+
+        Parameters
+        ----------
+        images: list
+        """
+        self.orig_images = images
+        self.keypoints = []
+        self.descriptors = []
+
+        for img in self.orig_images:
+            if len(img.shape) > 2:
+                if img.shape[-1] > 2:
+                    print("\t\tConverting to greyscale...")
+                    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                else:
+                    img = img[:, :, 0]  # ignore mask layer
+            if img.dtype != 'uint8':
+                print("\t\tConverting to 8-bit...")
+                img = self._conv_to_uint8(img)
+
+            features = self._detect_features(img)
+
+            self.keypoints.append(features['keypoints'])
+            self.descriptors.append(features['descriptors'])
+
+
+    """Predict image transforms that best align each image with the
+        Parameters
+        ----------
+        ref_image_index: int
+        reference image
+
+            Input image list index of the image to use as the reference image,
+            to which all other images are transformed
+        """
+    def predict(self, ref_image_index: int):
+        self.ref_image_index = ref_image_index
+
+        self.matches = []
+        self.transform_matrices = []
+        self.inlier_masks = []
+        self.inlier_points = []
+        for i in range(len(self.orig_images)):
+            if i == ref_image_index:
+                self.matches.append([])
+                continue
+            # find matching features
+            matches = self._match_desciptors(
+                self.descriptors[i], self.descriptors[ref_image_index]
+            )
+            self.matches.append(matches)
+
+            # estimate transform
+            keypoints = [self.keypoints[i], self.keypoints[ref_image_index]]
+            transform_output = self._predict_transform(matches, keypoints)
+            self.transform_matrices.append(transform_output['homography'])
+            self.inlier_masks.append(transform_output['inlier_mask'])
+            self.inlier_points.append(transform_output['inlier_points'])
+
+    def apply(self) -> List[np.array]:
+        """Warp images and return as a list"""
+        # warp images
+        final_images = []
+        for i, img in enumerate(self.orig_images):
+            if i == self.ref_image_index:
+                final_images.append(img)
+                continue
+
+            img_warped = cv2.warpPerspective(
+                img, self.transform_matrices[i - 1]  #, dsize=mos_shape_xy
+            )
+            final_images.append(img_warped)
+        return final_images
+
+    def _akaze(self, img):
+        """Detect keypoints and descriptors using AKAZE method"""
+        # AKAZE; G1 finds slightly more KPs than G2
+        finder = cv2.AKAZE.create(diffusivity=cv2.KAZE_DIFF_PM_G1,
+                                  threshold=self.detection_threshold)
+        kp, desc = finder.detectAndCompute(img, mask=None)
+        return {'keypoints': kp, 'descriptors': desc}
+
+    def _detect_features(self, img):
+        """Call specified feature detection method"""
+        if self.feature_method == 'akaze':
+            result = self._akaze(img)
+        else:
+            raise ValueError(
+                f"Feature detection method invalid: {self.feature_method}"
+            )
+        return result
+
+    def _match_desciptors(
+            self, desc_0, desc_1, method='BruteForce-Hamming', nn_k=2
+    ):
+        """Finds matching pairs between 2 sets of image feature descriptors.
+        Descriptors must be numpy arrays as per the standard opencv feature
+        detection output"""
+        matches = []
+        matcher = cv2.DescriptorMatcher_create(method)
+        # nearest neighbour matching
+        matches_tmp = matcher.knnMatch(desc_0, desc_1, k=nn_k)
+        for m in matches_tmp:
+            if m[0].distance < self.match_conf * m[1].distance:
+                matches.append(m[0])
+        matches = sorted(matches, key=lambda x: x.distance)  # Sort by distance
+        if len(matches) == 0:
+            raise Exception("No matches found between image pair")
+        return matches
+
+    def _predict_transform(self, matches, keypoints):
+        """Calculates 3x3 homography matrix that transforms an image onto base
+        image given the keypoints and matches
+
+        Parameters
+        ----------
+        matches : list of cv2.DMatch
+            all matches between image pair
+        keypoints : list of lists of cv2.KeyPoint
+            keypoints detected in each image [ [image1 kps], [image2 kps] ]
+        """
+        # get keypoints from matches
+        pts1 = []
+        pts2 = []
+        for m in matches:
+            pts1.append(keypoints[0][m.queryIdx].pt)
+            pts2.append(keypoints[1][m.trainIdx].pt)
+        pts1 = np.float32(pts1)
+        pts2 = np.float32(pts2)
+        # predict homography matrix
+        homography, inlier_mask = cv2.findHomography(
+            pts1, pts2, method=cv2.RANSAC
+        )
+        results = {
+            'homography': homography,
+            'inlier_mask': inlier_mask,
+            'inlier_points': (pts1, pts2)
+        }
+        return results
+
+    def _conv_to_uint8(self, img):
+        """Scales image pixel values to cover the range 0 -- 255, and
+        converts from current dtype to uint8.
+
+        Parameters
+        ----------
+        img : numpy.ndarray
+            image to rescale and convert to uint8
+
+        Returns
+        -------
+        numpy.ndarray
+            rescaled uint8 image.
+
+        """
+        max_pix = np.percentile(img, 99.99)  # ignore bright outliers
+        min_pix = img.min()
+        img = 255 * (img.astype(np.float32) - min_pix) / (max_pix - min_pix)
+        img = img.clip(0, 255).astype(np.uint8)
+        return img
