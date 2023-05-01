597961e2158b41835541366553a0bfbaa507f2d1
Elliott Polzin <elliott.polzin3@gmail.com>
2023-05-01 10:19:30 +0100

    finished read image function

--- src/imageprocess/utils/io.py
+++ src/imageprocess/utils/io.py
@@ -6,8 +6,11 @@ def read_image(img_path: str, bgr: bool = True) -> np.array:
 
     Parameters
     ----------
-    img_path : str
+    img_path: str
         File path to image file
+    bgr: bool, optional
+        Return BGR channels of the image as separate array dimensions, or
+        return single greyscale channel
 
     Returns
     -------
@@ -17,9 +20,12 @@ def read_image(img_path: str, bgr: bool = True) -> np.array:
     img = cv2.imread(img_path, -1)
     if img.shape[2] == 4:
         # set masked pixels to zero
-        img[img[: ,: ,3] == 0] = 0
-    if bgr:
+        img[img[:, :, 3] == 0] = 0
+    if bgr and img.shape[2] > 1:
         img = img[:, :, :3]
-    #ortho = cv2.cvtColor(ortho, cv2.COLOR_BGR2GRAY)
+    elif (not bgr) and img.shape[2] > 1:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    else:
+        pass
 
     return img
